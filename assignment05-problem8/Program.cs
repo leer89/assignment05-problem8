56aedcda4256bbacf4012e7dc2aefdd83b25e052
leer89 <leer89@gmail.com>
2017-02-27 20:32:50 -0500

    problem 8

--- assignment05-problem8/Program.cs
+++ assignment05-problem8/Program.cs
@@ -10,6 +10,38 @@ namespace assignment05_problem8
     {
         static void Main(string[] args)
         {
+            /* 8. Prompt the user for the length of three line segments as integers.If the three lines
+            could form a triangle, print the integers and a message indicating they form a
+            triangle. Use a state - controlled loop to allow users to enter as many different
+            combinations as they want. */
+
+            int side1, side2, side3;
+            string s1, s2, s3;
+            bool keepGoing = true;
+            string decision;
+
+            while (keepGoing)
+            {
+                Console.WriteLine("Give 3 sides and I will tell you if a triangle can be formed: ");
+                s1 = Console.ReadLine();
+                s2 = Console.ReadLine();
+                s3 = Console.ReadLine();
+
+                side1 = int.Parse(s1);
+                side2 = int.Parse(s2);
+                side3 = int.Parse(s3);
+
+                if ((side1 + side2) > side3)
+                {
+                    Console.WriteLine("Triangle is possible. Side1 + Side2 is larger than Side3.");
+                    Console.WriteLine("Try again? (n to exit): ");
+                    decision = Console.ReadLine();
+                    if (decision == "n")
+                        keepGoing = false;
+                }
+                else
+                    Console.WriteLine("Triangle not possible. Try again.");
+            }
         }
     }
 }
